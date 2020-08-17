b9ae495fc0ffe9af0be74de88202f3035d00556f
iamchenjiajun <iamchenjiajun@gmail.com>
2020-08-17 21:07:26 +0800

    Add greeting on launch (Level-0)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,20 @@
 public class Duke {
+
+    public static void showDivider() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I'm Duke!");
+        System.out.println("What can I do for you?");
+        showDivider();
+    }
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        showDivider();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +22,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        showDivider();
+        greet();
+        bye();
     }
+
 }
