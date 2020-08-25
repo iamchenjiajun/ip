265e9180516268bf149944feddb8e9bf38b2cab4
iamchenjiajun <iamchenjiajun@gmail.com>
2020-08-26 00:22:23 +0800

    Add REPL to process user input Echoes all user commands except "bye" which terminates the program.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
 
     public static void showDivider() {
@@ -5,6 +7,7 @@ public class Duke {
     }
 
     public static void greet() {
+        showDivider();
         System.out.println("Hello! I'm Duke!");
         System.out.println("What can I do for you?");
         showDivider();
@@ -12,7 +15,10 @@ public class Duke {
 
     public static void bye() {
         System.out.println("Bye. Hope to see you again soon!");
-        showDivider();
+    }
+
+    public static void echo(String line) {
+        System.out.println(line);
     }
 
     public static void main(String[] args) {
@@ -23,9 +29,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        showDivider();
+        Scanner in = new Scanner(System.in);
+        boolean isBye = false;
+
         greet();
-        bye();
+
+        while (!isBye) {
+            String line = in.nextLine();
+            showDivider();
+
+            switch(line) {
+            case "bye":
+                isBye = true;
+                bye();
+                break;
+            default:
+                echo(line);
+                break;
+            }
+
+            showDivider();
+        }
     }
 
 }
