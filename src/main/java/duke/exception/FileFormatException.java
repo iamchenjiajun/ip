7213bbc4aad15dd95d1c0aa049e5528c7f2aacb2
iamchenjiajun <iamchenjiajun@gmail.com>
2020-09-29 21:23:10 +0800

    Add JavaDoc for DukeException and its subclasses.

--- src/main/java/duke/exception/FileFormatException.java
+++ src/main/java/duke/exception/FileFormatException.java
@@ -1,4 +1,7 @@
 package duke.exception;
 
+/**
+ * Represents an exception thrown when there is an error in the formatting of file contents.
+ */
 public class FileFormatException extends DukeException {
 }
--- src/main/java/duke/exception/UnknownCommandException.java
+++ src/main/java/duke/exception/UnknownCommandException.java
@@ -2,8 +2,15 @@ package duke.exception;
 
 import duke.ui.Ui;
 
+/**
+ * Represents an exception thrown when the user input contains an unknown command.
+ */
 public class UnknownCommandException extends DukeException {
-
+    /**
+     * Returns the error message.
+     *
+     * @return Error message.
+     */
     public String getErrorMessage() {
         return Ui.ERROR_UNKNOWN_COMMAND;
     }
